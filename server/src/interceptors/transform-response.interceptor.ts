1f4a42c60cbc031b7798720c0a28cbd54eaac2b9
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2025-03-12 02:04:08 +0200

    feat: create a interceptor to transform the response

---
+++ server/src/interceptors/transform-response.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  code: number;
+  isError: boolean;
+  status: string;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class TransformResponseInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        const response = {
+          code: 200,
+          isError: data == null,
+          status: 'OK',
+          message: 'success',
+          data,
+        };
+
+        return response;
+      }),
+    );
+  }
+}
